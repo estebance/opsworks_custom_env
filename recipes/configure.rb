cba333a5a7c9ff5c07eada58aab31c4823c4bf34
estebance <restebance@gmail.com>
2018-04-02 18:41:50 -0500

    generate recipe to compile assets

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -2,5 +2,5 @@ if node[:opsworks][:instance][:layers].include?('rails-app')
 
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
-
+  include_recipe "opsworks_custom_env::compile_assets"
 end
